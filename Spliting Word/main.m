e2cbcca426db7cc9e40e7e5a8e5d19175143ff09
Chu Nam Anh <khuyendaxoa1996@yahoo.com.vn>
2016-06-12 17:01:11 +0700

    Initial Commit

---
+++ Spliting Word/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Spliting Word
+//
+//  Created by Chu Nam Anh on 6/12/16.
+//  Copyright © 2016 Chu Nam Anh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
